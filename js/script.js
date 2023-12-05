232be8218ac99d84e2559fa39f7d4d5e4d714f1d
andi <echo.matuladaandi@gmail.com>
2023-12-06 06:57:47 +0800

    slidebar hamburger menu ketika di layout dgn ukuran tertentu akan muncul dan hilang

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav"); // cari class navbar-nav
+
+// ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle("active"); // muncul class active kalau tidak ada dan hilang kalau ada, ketika hamburger menu diklik
+};
+
+// klik di luar slidebar utk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+// ketika di klik di mana saja diluar slidebar / hamburgermenu
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+
